48431f3522eccd38c1d23a031bf584b53724e934
Ferriel Melarpis <ferriel.melarpis@freedom.tm>
2018-01-15 15:00:24 +0800

    Added es6 env to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     env: {
         browser: true,
         node: true,
+        es6: true,
     },
     extends: [
         // https://github.com/eslint/eslint/blob/master/conf/eslint-recommended.js
