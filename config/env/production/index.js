35c7cf56b31d6f9b1fb1a269d2595e8f37730858
Ferriel Melarpis <ferriel.melarpis@freedom.tm>
2018-01-16 17:40:11 +0800

    Removed production config

--- config/env/production/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @WARNING: production env config should be put to a submodule
- */
-const { stringifyConfig } = require('../../../build/config-utils');
-
-module.exports = stringifyConfig({
-    NODE_ENV: 'production'
-});
-
