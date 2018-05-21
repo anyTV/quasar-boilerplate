ba831226f793cd687b10e111551bbdf5ab433cc9
Ferriel Melarpis <ferriel.melarpis@freedom.tm>
2018-05-21 17:34:04 +0800

    Removed sample module

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,11 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-import example from './module-example'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  modules: {
-    example
-  }
-})
+  modules: {}
+});
 
-export default store
+export default store;
