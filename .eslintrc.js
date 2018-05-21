55503b1ad6b777b6b17c001e7ebd4a6b4ed75005
Ferriel Melarpis <ferriel.melarpis@freedom.tm>
2018-05-21 16:31:12 +0800

    Updated .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,7 @@ module.exports = {
         sourceType: 'module'
     },
     env: {
-        browser: true,
-        node: true,
-        es6: true,
+        browser: true
     },
     extends: [
         // https://github.com/eslint/eslint/blob/master/conf/eslint-recommended.js
@@ -20,28 +18,36 @@ module.exports = {
     ],
     // required to lint *.vue files
     plugins: [
-        'html',
-        'import'
+        'vue',
+        'import',
     ],
     globals: {
+        'ga': true, // Google Analytics
         'cordova': true,
-        'DEV': true,
-        'PROD': true,
-        '__THEME': true
+        '__statics': true,
+        'process': true,
     },
     // add your custom rules here
     'rules': {
+        // allow async-await
+        'generator-star-spacing': 'off',
+
         // allow paren-less arrow functions
-        'arrow-parens': 0,
-        'one-var': 0,
-        'import/first': 0,
-        'import/named': 2,
-        'import/namespace': 2,
-        'import/default': 2,
-        'import/export': 2,
+        'arrow-parens': 'off',
+        'one-var': 'off',
+
+        'import/first': 'off',
+        'import/named': 'error',
+        'import/namespace': 'error',
+        'import/default': 'error',
+        'import/export': 'error',
+        'import/extensions': 'off',
+        'import/no-unresolved': 'off',
+        'import/no-extraneous-dependencies': 'off',
+
         // allow console and debugger during development
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-        'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
+        'brace-style': ['error', 'stroustrup', { 'allowSingleLine': true }]
     }
 };
