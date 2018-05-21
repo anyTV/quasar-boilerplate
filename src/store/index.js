c7872259ae7ed0e357f97c4cb6de179349372099
Ferriel Melarpis <ferriel.melarpis@freedom.tm>
2018-05-21 17:51:31 +0800

    Use strict mode in Vuex when not in prod mode

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,8 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  modules: {}
+  modules: {},
+  strict: process.env.NODE_ENV !== 'production'
 });
 
 export default store;
