54d1b92d4f1cd5b9d6fdfd43c7e944ce129b6657
Ferriel Melarpis <ferriel.melarpis@freedom.tm>
2018-05-25 15:05:26 +0800

    indent

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,50 +1,50 @@
 <template>
-  <q-page padding>
-    <div class="row">
-      <div class="col-4">
-        <q-card>
-          <language-selector header="locales">
-            <template
-              slot="languages"
-              slot-scope="{ changeLanguage }"
-            >
-              <q-item
-                v-for="lang in languages"
-                :key="lang.value"
-                @click.native="changeLanguage(lang.value)"
-              >
-                <q-item-side :icon="lang.icon" />
-                <q-item-main :label="$t(lang.label)" />
-              </q-item>
-            </template>
-          </language-selector>
-          <q-card-main>
-            {{ $t('sample') }}
-          </q-card-main>
-        </q-card>
-      </div>
-    </div>
-  </q-page>
+    <q-page padding>
+        <div class="row">
+            <div class="col-4">
+                <q-card>
+                    <language-selector header="locales">
+                        <template
+                            slot="languages"
+                            slot-scope="{ changeLanguage }"
+                        >
+                            <q-item
+                                v-for="lang in languages"
+                                :key="lang.value"
+                                @click.native="changeLanguage(lang.value)"
+                            >
+                                <q-item-side :icon="lang.icon" />
+                                <q-item-main :label="$t(lang.label)" />
+                            </q-item>
+                        </template>
+                    </language-selector>
+                    <q-card-main>
+                        {{ $t('sample') }}
+                    </q-card-main>
+                </q-card>
+            </div>
+        </div>
+    </q-page>
 </template>
 
 <script>
-  import LanguageSelector from '@/LanguageSelector';
+    import LanguageSelector from 'components/LanguageSelector';
 
-  export default {
-    name: 'Home',
-    components: {
-      LanguageSelector,
-    },
-    computed: {
-      languages() {
-        return [
-          { label: 'english', value: 'en', icon: 'flag' },
-          { label: 'thai', value: 'th', icon: 'flag' },
-          { label: 'japanese', value: 'jp', icon: 'flag' }
-        ];
-      }
-    }
-  };
+    export default {
+        name: 'Home',
+        components: {
+            LanguageSelector,
+        },
+        computed: {
+            languages() {
+                return [
+                    { label: 'english', value: 'en', icon: 'flag' },
+                    { label: 'thai', value: 'th', icon: 'flag' },
+                    { label: 'japanese', value: 'jp', icon: 'flag' }
+                ];
+            }
+        }
+    };
 </script>
 
 <style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,9 @@
 export default [
-  {
-    path: '/',
-    component: () => import('layouts/Home'),
-    children: [
-      { path: '', name: 'Home', component: () => import('pages/Home')}
-    ]
-  }
+    {
+        path: '/',
+        component: () => import('layouts/Home'),
+        children: [
+            { path: '', name: 'Home', component: () => import('pages/Home') }
+        ]
+    }
 ];
