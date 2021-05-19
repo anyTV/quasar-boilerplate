da7f573a985a484b89fa611cdda825bf2e1c4e41
Jonel Rempis <Jonel.Rempis@freedom.tm>
2021-05-19 21:07:23 +0800

    Dev-3801 Dependency Issue > Lock all codebase vue and vue compiler version (#35)

--- src/boot/vue-i18next.js
+++ src/boot/vue-i18next.js
@@ -1,5 +1,5 @@
 import i18next from 'i18next';
-import i18nextXHRBackend from 'i18next-xhr-backend';
+import i18nextHttpBackend from 'i18next-http-backend';
 import i18nextLangDetector from 'i18next-browser-languagedetector';
 import VueI18next from '@panter/vue-i18next';
 import i18nextConfig from 'src/config/i18next';
@@ -33,7 +33,7 @@ export default ({ app, Vue }) => {
     i18next.use(i18nextLangDetector);
 
     if (i18nextConfig.backend) {
-        i18next.use(i18nextXHRBackend);
+        i18next.use(i18nextHttpBackend);
     }
 
     i18next.init(i18nextConfig);
